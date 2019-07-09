b1b69a8ed0b923ee94600b337fbad1b2e4fcb329
baymin <yiningzeng@outlook.com>
2019-07-09 16:32:26 +0800

    fix: 保存输出的格式

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -140,7 +140,7 @@ export default class ProjectService implements IProjectService {
 
         await storageProvider.writeText(
             `${project.name}${constants.projectFileExtension}`,
-            JSON.stringify(project, null, 4),
+            JSON.stringify(project, null, "\t"),
         );
 
         return project;
