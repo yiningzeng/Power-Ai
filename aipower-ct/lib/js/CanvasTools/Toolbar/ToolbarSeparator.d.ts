ade1cbbf353919596ea8ed3380e6f1a1cd372069
yiningzeng <yiningzeng@outlook.com>
2019-09-20 17:12:53 +0800

    feat: 删除多余的

--- aipower-ct/lib/js/CanvasTools/Toolbar/ToolbarSeparator.d.ts
+++
@@ -1,8 +0,0 @@
-import { ToolbarIcon } from "./ToolbarIcon";
-export declare class ToolbarSeparator extends ToolbarIcon {
-    private iconSeparator;
-    constructor(paper: Snap.Paper, width: number);
-    move(x: number, y: number): void;
-    resize(width: number, height: number): void;
-    private buildIconUI;
-}
